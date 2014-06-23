65d5419e1eaacf20c739629649c3cd440ebc13b6
murayama <murayama333@gmail.com>
2014-06-23 00:48:50 +0000

    add better_errors gem.

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+  def index
+    # name = "murayama"
+    name.upcase!
+  end
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  BetterErrors::Middleware.allow_ip! ENV['AWESOME_TRUSTED_IP'] if ENV['AWESOME_TRUSTED_IP']
+
 end
